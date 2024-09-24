2f6af1f7187906f6073736e8e5337cf00af47df6
Alexander <fix1in1it@gmail.com>
2024-09-24 12:26:31 +0300

    after review

--- frontend/bmstu_app/views.py
+++ frontend/bmstu_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.conf import settings
 
-mockSection1 = {
+sections = [
+  {
   'id': 1,
   'name': 'Футбол для начинающих',
   'description': 'Курс для тех, кто только начинает заниматься футболом.',
@@ -10,9 +11,8 @@ mockSection1 = {
   'instructor': 'Иванова Елена Петровна',
   'duration': '1,5 часа',
   'imageUrl': 'http://127.0.0.1:9000/bmstu-sport/football.png'
-}
-
-mockSection2 = {
+},
+  {
   'id': 2,
   'name': 'Баскетбол \'Техника бросков\'',
   'description': 'Курс по совершенствованию техники бросков в баскетболе.',
@@ -21,9 +21,8 @@ mockSection2 = {
   'instructor': 'Иванова Елена Петровна',
   'duration': '1,5 часа',
   'imageUrl': 'http://127.0.0.1:9000/bmstu-sport/basketball.png'
-}
-
-mockSection3 = {
+},
+  {
   'id': 3,
   'name': 'Хоккей',
   'description': 'Курс по улучшению техники плавания на стиле брасса.',
@@ -32,9 +31,8 @@ mockSection3 = {
   'instructor': 'Иванова Елена Петровна',
   'duration': '1,5 часа',
   'imageUrl': 'http://127.0.0.1:9000/bmstu-sport/hockey.png'
-}
-
-mockSection4 = {
+},
+  {
   'id': 4,
   'name': 'Картинг',
   'description': 'Курс по обучению хип-хоп танцам для детей 6-10 лет.',
@@ -43,9 +41,8 @@ mockSection4 = {
   'instructor': 'Иванова Елена Петровна',
   'duration': '1,5 часа',
   'imageUrl': 'http://127.0.0.1:9000/bmstu-sport/racing.png'
-}
-
-mockSection5 = {
+},
+  {
   'id': 5,
   'name': 'Подготовка к марафону',
   'description': 'Курс для тех, кто только начинает заниматься фитнесом и строить мышцы.',
@@ -54,17 +51,13 @@ mockSection5 = {
   'instructor': 'Иванова Елена Петровна',
   'duration': '1,5 часа',
   'imageUrl': 'http://127.0.0.1:9000/bmstu-sport/cycling.png'
-}
-
-sections = [
-  mockSection1,
-  mockSection2,
-  mockSection3,
-  mockSection4,
-  mockSection5,
+},
 ]
 
-mock_section_order_1 = [
+mock_application_1 = {
+    'id': 1,
+    'fio': '',
+    'section_order': [
    {
       'id': 1,
       'id_application': 1,
@@ -84,24 +77,17 @@ mock_section_order_1 = [
       'order_number': 2
    },
 ]
-
-mock_application_1 = {
-    'id': 1,
-    'fio': '',
-    'section_order': mock_section_order_1,
 }
-
-applications = [
-  mock_application_1
-]
 
 def index(request):
     search_query = request.GET.get('section_name', '')
     filtered_sections = [section for section in sections if search_query.lower() in section['name'].lower()]
+    application_sections_size = len(mock_application_1['section_order'])
     
     context = {
         'sections': filtered_sections,
-        'application': mock_application_1
+        'application': mock_application_1,
+        'application_sections_counter': application_sections_size
     }
     return render(request, 'index.html', context)
 
@@ -121,16 +107,7 @@ def section(request, section_id):
     return render(request, 'section.html', context)
 
 def application(request, application_id):
-    searched_application = None
-    for application in applications:
-      if application['id'] == application_id:
-        searched_application = application
-        break
-    
-    if searched_application == None:
-      return
-
-    sorted_sections_order = sorted(searched_application['section_order'], key=lambda dict: dict['order_number'])
+    sorted_sections_order = sorted(mock_application_1['section_order'], key=lambda dict: dict['order_number'])
 
     sorted_sections = []
     index = 0
@@ -142,8 +119,8 @@ def application(request, application_id):
             break
 
     context = {
-      'id': searched_application['id'],
-      'fio': searched_application['fio'],
+      'id': mock_application_1['id'],
+      'fio': mock_application_1['fio'],
       'sections': sorted_sections,
     }
 
